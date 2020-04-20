429dc84959c5e6a8f51ff66e3259416fd9bdc685
ycs5682 <c-yu@creationline.com>
2020-04-20 16:22:44 +0900

    確率を操作する

--- js/main.js
+++ js/main.js
@@ -7,16 +7,13 @@
     const results = ['大吉','中吉','凶','末吉'];
     btn.textContent = results[Math.floor(Math.random() * results.length)];
 
-    // switch (n) {
-    //   case 0:
-    //     btn.textContent = '大吉';
-    //     break;
-    //   case 1:
-    //     btn.textContent = '中吉';
-    //     break;
-    //   case 2:
-    //     btn.textContent = '凶';
-    //     break;
-    // }
+    const n = Math.random();
+    if(n < 0.05) {
+      btn.textContent = '大吉';
+    } else if (n < 0.2) {
+      btn.textContent = '中吉';
+    } else {
+      btn.textContent = '凶';
+    }
   });
 }
